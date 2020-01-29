dc4d55c0925d742fdaa091b6f9bd9e66280ad789
lgb211 <60093243+lgb211@users.noreply.github.com>
2020-01-29 22:47:30 +0900

    String Repeat

---
+++ StringRepeat.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class StringRepeat {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	
+		sc.nextLine();
+	
+ 		for(int i =0; i<n;i++) {
+			
+			String b[] = sc.nextLine().split(" ");
+			
+			int c = new Integer(b[0]).intValue();
+			String[] a = b[1].split("");
+			
+	    	for(int l=0;l<a.length;l++) {	
+			for(int j =0;j<c;j++) {
+					System.out.print(a[l]);
+			}
+	    	}
+	    	System.out.println();
+ 		}
+	
+ 		
+		sc.close();
+		}
+}
