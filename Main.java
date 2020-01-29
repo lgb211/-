a75b3a44a25685403e09b4d153c7bdb89b7defcb
lgb211 <60093243+lgb211@users.noreply.github.com>
2020-01-29 22:09:29 +0900

    OX quiz

--- Main.java
+++ Main.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
 public class Main {
+public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
+	Scanner sc = new Scanner(System.in);
+	int n =sc.nextInt();
+	String [] b = new String[n];
+	int sum=0,pl=0;
+	for(int i=0;i<n;i++) {
+		b[i] = sc.next();
+		for(int j = 0; j<b[i].length();j++) {
+			 if(b[i].charAt(j)=='O') {
+				pl++;
+				 sum += pl;
+			 }else {
+				 pl = 0;
+			 }
+		 }
+	System.out.println(sum);
+	sum=0;
+	pl=0;
+	}
+	sc.close();
 	
-		sc.nextLine();
-	
- 		for(int i =0; i<n;i++) {
-			
-			String b[] = sc.nextLine().split(" ");
-			
-			int c = new Integer(b[0]).intValue();
-			String[] a = b[1].split("");
-			
-	    	for(int l=0;l<a.length;l++) {	
-			for(int j =0;j<c;j++) {
-					System.out.print(a[l]);
-			}
-	    	}
-	    	System.out.println();
- 		}
-	
- 		
-		sc.close();
-		}
+}
 }
