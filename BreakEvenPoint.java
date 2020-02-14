1b41e3f84a94398548b7fe232dfd45b50b7f4197
lgb211 <60093243+lgb211@users.noreply.github.com>
2020-02-15 02:40:52 +0900

    백준 1712 손익분기점

---
+++ BreakEvenPoint.java
@@ -0,0 +1,27 @@
+package Breakevenpoint;
+import java.util.*;
+public class BreakEvenPoint {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	String [] value = sc.nextLine().split(" "); 
+	
+	int lockup = 0,variable = 0,sale = 0,sum=0,count = 1;
+	
+	for(int i = 0 ; i<value.length;i++) {
+		 if(i ==0) 
+		 lockup = Integer.parseInt(value[i]);
+		 if(i == 1) 
+		 variable = Integer.parseInt(value[i]);
+		 if(i == 2)
+		 sale = Integer.parseInt(value[i]);
+		
+	}
+	if(variable>=sale)
+		System.out.println(-1);
+	else
+		System.out.println((lockup/(sale-variable))+1);
+
+	sc.close();
+}
+}
